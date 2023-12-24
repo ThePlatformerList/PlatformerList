aa609fc1c96c2f6a21b266720a4751dd0a38d41b
gdhpsk <70782191+gdhpsk@users.noreply.github.com>
2023-12-24 16:39:54 -0500

    Update leaderboard.js

--- schemas/leaderboard.js
+++ schemas/leaderboard.js
@@ -1,14 +1,8 @@
 const mongoose = require("mongoose")
 
-const worldRecordSchema = new mongoose.Schema({
-    levelID: String,
-    link: String,
-    time: Number
-})
-
 const leaderboardSchema = new mongoose.Schema({
     name: String,
-    records: [worldRecordSchema]
+    records: [mongoose.Types.ObjectId]
 })
 
 module.exports = mongoose.model("leaderboards", leaderboardSchema)
