1323d0d042e3d3565710be0d49495d89cf042358
gdhpsk <70782191+gdhpsk@users.noreply.github.com>
2025-04-01 21:54:42 -0700

    Update Spinner.js

--- vueFiles/js/components/Spinner.js
+++ vueFiles/js/components/Spinner.js
@@ -1,3 +1,3 @@
 export default {
-    template: `<img src="https://cdn.discordapp.com/attachments/1192608864370167909/1356476744655306792/352700595-d680ddc0-d157-48a4-a884-4bcfbd8bfb01.png?ex=67ecb4ea&is=67eb636a&hm=6406ae971557ca9a2deb21f521a0184bb380e558f26d7a210348f5ee34285afb&" class="spinner"></img>`,
+    template: `<p class="spinner">Loading...</p>`,
 };
